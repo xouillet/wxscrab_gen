e6d8db75600d382d40c9c3947a9bdb2aefa73b51
Philippe Chataignon <philippe@chataignon.com>
2020-04-22 23:46:06 +0200

    Amorce serveur bootle

---
+++ server/server.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import bottle
+from bottle import route, run, template, static_file, response, abort
+import xml.etree.cElementTree as ET
+import random
+import subprocess
+
+app = application = bottle.Bottle()
+data = {}
+
+@app.route("/gen_part/<dico>")
+def gen_part(dico):
+    r = random.SystemRandom()
+    pgm = "../gen/gen_part"
+    num = r.randrange(0,2**32)
+    seed = r.randrange(0,2**16)
+    p = subprocess.Popen([pgm, "-d", f"../gen/{dico}.dico", "-n", str(num), "-s", str(seed)], stdout=subprocess.PIPE)
+    result = p.communicate()[0]
+    response.content_type = 'text/xml'
+    return result
+
+def test_gen_part(dico):
+    r = random.SystemRandom()
+    pgm = "../gen/gen_part"
+    num = r.randrange(0,2**32)
+    seed = r.randrange(0,2**16)
+    #response.content_type = 'application/json'
+    #return json.dumps(ret)
+
+
+if __name__ == '__main__':
+    run(app, host='0.0.0.0', port=8080)
+    # print(test_gen_part("ods8"))
